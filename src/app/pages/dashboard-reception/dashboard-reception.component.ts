3a48c0b83f7ab2714ab44897bc5ba848eb8ba2b9
parantap-pt <hiralpatel133@gmail.com>
2022-01-18 19:08:26 +0530

    code

--- src/app/pages/dashboard-reception/dashboard-reception.component.ts
+++ src/app/pages/dashboard-reception/dashboard-reception.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../service/auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CommonService } from '../../service/common.service';
+import { ToastrService } from 'ngx-toastr';
+import { FormGroup,FormBuilder,Validators } from '@angular/forms';
+import { NgxSpinnerService } from "ngx-spinner";
 
 @Component({
   selector: 'app-dashboard-reception',
@@ -7,9 +13,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardReceptionComponent implements OnInit {
 
-  constructor() { }
+  public Constant : any;
+
+  public booking: any = {data : []};
+
+  constructor(public commonservice:CommonService,private activatedRoute: ActivatedRoute,private router: Router,public fb: FormBuilder,public toastr : ToastrService,private spinner: NgxSpinnerService, public authService:AuthService) { 
+    
+    this.Constant = this.commonservice.getConstants();
+
+  	//meta tags set
+    var meta_tag = [
+      {name : 'og:title' , content : 'Dashboard'}
+    ];
+    this.commonservice.changeMetaTagOfPage(meta_tag);
+    this.commonservice.setTitle('Dashboard');
+    //meta tags set
+  }
 
   ngOnInit(): void {
+    this.loadAllBooking();
+  }
+
+  public loadAllBooking(){
+
+    let body = new FormData();
+    body.append('hotel_id', '1');
+    body.append('token', this.Constant['API_TOKEN']);
+
+    
+    let options = this.commonservice.generateRequestHeaders(false);
+    this.commonservice.SubmiPostFormData('get_booking_details',body,options)
+    .then((response) => {  
+      //console.log(response.data);        
+      if(response.status == true){
+        if(response.data.length  > 0){
+          this.booking.data = response.data;
+        }
+        else{
+          this.booking.data = [];
+        }
+      }  
+      else{
+        this.booking.data = [];
+      }  
+    })
+    .catch((error) => {
+      console.log(error);
+      return false;
+    });
   }
 
 }
