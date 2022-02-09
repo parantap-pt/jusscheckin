eb346bc9636fa081dadf3afe7637e9d9a6616cc8
parantap-pt <hiralpatel133@gmail.com>
2022-02-09 19:32:34 +0530

    code

--- src/app/pages/add-rooms/add-rooms.component.ts
+++ src/app/pages/add-rooms/add-rooms.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CommonService } from '../../service/common.service';
+import { AuthService } from '../../service/auth.service';
+import { ToastrService } from 'ngx-toastr';
+import { FormGroup,FormBuilder,Validators } from '@angular/forms';
+import { NgxSpinnerService } from "ngx-spinner";
 
 @Component({
   selector: 'app-add-rooms',
@@ -7,9 +13,95 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddRoomsComponent implements OnInit {
 
-  constructor() { }
+  public Constant : any;
+  public property: any;
+  public frmAddRooms: FormGroup;
+
+  constructor(public commonservice:CommonService,private activatedRoute: ActivatedRoute,private router: Router,public fb: FormBuilder,public toastr : ToastrService,private spinner: NgxSpinnerService,public authService:AuthService) { 
+    this.Constant = this.commonservice.getConstants();
+    //meta tags set
+     var meta_tag = [
+       {name : 'og:title' , content : 'Add Rooms '+' - '+this.Constant['SITE_NM']}
+     ];
+     this.commonservice.changeMetaTagOfPage(meta_tag);
+     this.commonservice.setTitle('Add Rooms');
+     //meta tags set
+
+      this.frmAddRooms = fb.group({
+       'property_id' : ['', [Validators.required]],
+       'room_type' : ['', [Validators.required]],
+       'room_number' : ['', [Validators.required]],
+       'room_price' : ['', [Validators.required]]
+     });
+  }
 
   ngOnInit(): void {
+    this.loadAllProperty();
+  }
+
+  public loadAllProperty(){
+
+    let body = new FormData();
+    body.append('user_id', this.authService.loggedInUserId);
+    body.append('token', this.Constant['API_TOKEN']);
+
+    
+    let options = this.commonservice.generateRequestHeaders(false);
+    this.commonservice.SubmiPostFormData('get_my_property',body,options)
+    .then((response) => {  
+      //console.log(response.data);        
+      if(response.status == true){
+        if(response.data.length  > 0){
+          this.property = response.data;
+        }
+        else{
+          this.property = [];
+        }
+      }  
+      else{
+        this.property = [];
+      }  
+    })
+    .catch((error) => {
+      console.log(error);
+      return false;
+    });
+  }
+
+  submitAddRoom() { 
+    this.spinner.show();
+    
+    let body = new FormData();
+    body.append('property_id', this.frmAddRooms.value.property_id);
+    body.append('room_type', this.frmAddRooms.value.room_type);
+    body.append('room_number', this.frmAddRooms.value.room_number);
+    body.append('room_price', this.frmAddRooms.value.room_price);
+    body.append('user_id', this.authService.loggedInUserId);
+    body.append('token', this.Constant['API_TOKEN']);
+
+    let options = this.commonservice.generateRequestHeaders();
+    this.commonservice.SubmiPostFormData('add_rooms',body,options)
+    .then((response) => {     
+      this.spinner.hide();
+      if(response.status == true){
+        
+        this.toastr.success(response.message);
+        this.frmAddRooms.reset();
+        //this.router.navigate(['/manage-task']);
+        return true;
+        
+      }else{
+        if(response.message != ''){
+          this.toastr.error(response.message);        
+        }
+        return false;
+      }  
+    }).catch((error) => {
+      console.log(error);
+      this.toastr.error('Something went wrong');
+      return false;
+    });
+    
   }
 
 }
