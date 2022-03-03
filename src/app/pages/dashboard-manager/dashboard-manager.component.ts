28bc855ccc5cc84c797cd039a154bf77a2bd0a0a
parantap-pt <hiralpatel133@gmail.com>
2022-03-03 17:53:20 +0530

    code update

--- src/app/pages/dashboard-manager/dashboard-manager.component.ts
+++ src/app/pages/dashboard-manager/dashboard-manager.component.ts
@@ -15,9 +15,11 @@ import { NgxSpinnerService } from "ngx-spinner";
 export class DashboardManagerComponent implements OnInit {
 
   public Constant : any;
-  public csValid = new customValidations();
+  public property: any;
+  public frmProperty: FormGroup;
+  public property_data: any = {data : []};
 
-   constructor(public commonservice:CommonService) { 
+   constructor(public commonservice:CommonService,private activatedRoute: ActivatedRoute,private router: Router,public fb: FormBuilder,public toastr : ToastrService,private spinner: NgxSpinnerService, public authService:AuthService) { 
    	  
    	   this.Constant = this.commonservice.getConstants();
 	   //meta tags set
@@ -28,8 +30,71 @@ export class DashboardManagerComponent implements OnInit {
 	    this.commonservice.setTitle('Dashboard');
 	    //meta tags set
 
+		this.frmProperty = fb.group({
+			'property_id' : ['', [Validators.required]]
+		});
+
 	 }
 
   ngOnInit(): void {
+	this.loadAllProperty();
+  }
+
+  public loadAllProperty(){
+
+    let body = new FormData();
+    body.append('user_id', this.authService.loggedInUserId);
+    body.append('token', this.Constant['API_TOKEN']);
+
+    
+    let options = this.commonservice.generateRequestHeaders(false);
+    this.commonservice.SubmiPostFormData('get_my_property',body,options)
+    .then((response) => {  
+      //console.log(response.data);        
+      if(response.status == true){
+        if(response.data.length  > 0){
+          this.property = response.data;
+        }
+        else{
+          this.property = [];
+        }
+      }  
+      else{
+        this.property = [];
+      }  
+    })
+    .catch((error) => {
+      console.log(error);
+      return false;
+    });
+  }
+
+  submitProperty(){
+    let body = new FormData();
+    body.append('user_id', this.authService.loggedInUserId);
+    body.append('property_id', this.frmProperty.value.property_id);
+    body.append('token', this.Constant['API_TOKEN']);
+
+    let options = this.commonservice.generateRequestHeaders();
+    this.commonservice.SubmiPostFormData('onwer_dashboard',body,options)
+    .then((response) => {     
+      this.spinner.hide();
+      if(response.status == true){
+        if(response.data.length  > 0){
+          console.log(response.data[0].available_room)
+          this.property_data = response.data[0];
+        }
+        else{
+          this.property_data = [];
+        }
+      }  
+      else{
+        this.property_data = [];
+      }  
+    }).catch((error) => {
+      console.log(error);
+      this.toastr.error('Something went wrong');
+      return false;
+    });
   }
 }
